74e31cb5f024a182ed8d7e5da169576739e77d55
PascalArvieu <pascalarvieu@gmail.com>
2020-11-03 10:49:01 +0100

    exercice 4

---
+++ Assets/JS/script.js
@@ -0,0 +1,33 @@
+// // Exercice 1
+// var coincoin = "Bien penser à mettre la ligne \"script type\" en bas, juste avant la fermeture de la balise \"body\"";
+// alert(coincoin)
+
+// // Exercice 2
+// var lastName = "John";
+// var firstName = "Doe";
+// var city = "Chicago";
+//     alert("Nom : " + lastName + "\n" + "Prénom : " + firstName + "\n" + "Ville : " + city);
+
+
+// Exercice 3
+// var firstName = prompt("C'est quoi ton blaze ?", "<Entrez ici votre prénom>");
+//     alert("Bonjour " + firstName + ", ça boume ?");
+
+// Exercice 4
+function OKgood() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var city = document.getElementById("city").value;
+
+    alert("Prénom : " + firstName + "\n" + "Nom : " + lastName + "\n" + "Ville : " + city);
+}
+
+
+// var lastName = prompt("Nom : ");
+// var firstName = prompt("Prénom : ");
+// var city = prompt("Ville : ");
+//     alert ("Nom : " + lastName + "\n" + "Prénom : " + firstName + "\n" + "Ville : " + city);
+
+// Exercice 4 correction
+
+    
